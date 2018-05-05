347885aa098eb40a839fb349d936d3bddde72906
Aniruddh Khera <khera.aniruddh@gmail.com>
2018-05-05 08:15:31 -0400

    REMOVED API TOKEN

--- prisma2.py
+++ prisma2.py
@@ -1,9 +1,9 @@
 import dropbox
 
-dbx = dropbox.Dropbox('56Avmht1CGkAAAAAAAABS8gYYmjrmVC5HZc6ThJbMT168Y1MrWhY0wjfXUln64i4')
+dbx = dropbox.Dropbox('<API_TOKEN>')
 print(dbx.users_get_current_account())
 
-f= open("/Users/aniruddhkhera/Downloads/googly.jpg","rb")
+f= open("~/Downloads/googly.jpg","rb")
 
 # to upload (content, dest)
 dbx.files_upload(f.read(), "/prisma/google.jpg")
@@ -12,4 +12,4 @@ for entry in dbx.files_list_folder('/prisma').entries:
     print(entry.name)
 
 
-dbx.files_download_to_file("/Users/aniruddhkhera/Desktop/googly.jpg", "/prisma/google.jpg")
+dbx.files_download_to_file("~/Desktop/googly.jpg", "/prisma/google.jpg")
