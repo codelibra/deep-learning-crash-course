5f68292f99ac0533aa50dc082c3ef48f1ed69009
Aniruddh Khera <aniruddh.khera@nyu.edu>
2018-05-11 13:51:27 -0400

    fixes for filter imgs from dbx and other ui change

--- prisma/App/web-app/src/components/Form.js
+++ prisma/App/web-app/src/components/Form.js
@@ -5,27 +5,46 @@ import uuid from 'uuid';
 import Dropzone from 'react-dropzone';
 import './dropzone-override.css';
 import LoadingSpinner from './LoadingSpinner';
-
-
-import {Alert} from 'react-bootstrap';
-
+import {Button, Alert,Carousel} from 'react-bootstrap';
 class Form extends Component {
    constructor(props) {
+
     super(props);
     this.state = { files: [],
                   loading: false, // will be true when ajax request is running
                   alert: false,
                   response: "",
+                  filters:[],
+                  emailId:"",
                   filterName:"",
-                  emailId:""
+                  bgColor:"gray"
       };
-   }
-      
-	 static defaultProps = {
-    filters: ['Wave', 'Modern Art', 'Painting']
-  }
+   }  
+
+  fetchFilterImages() {
+  var filters=[];
+  dbx.filesListFolder({path: '/prisma/styles'})
+  .then(function(resp){ 
+    resp.entries.forEach(function(element) {
+      dbx.filesDownload({path:element.path_display}).
+      then(function(image) {
+        filters.push({img: window.URL.createObjectURL(image.fileBlob), name: image.name});      
+      });
+});
 
 
+    })
+   .catch(function(error) {
+            console.error(error);
+          });
+
+   this.setState({filters:filters});
+}
+
+
+componentWillMount() {
+this.fetchFilterImages();
+}
 
 onDrop(files) {
     this.setState({
@@ -35,17 +54,14 @@ onDrop(files) {
 
 
   handleSubmit(e) {
-    console.log(this.refs.filter.value);
-   this.setState({filterName: this.refs.filter.value});
    const data = new FormData();
    data.append('contentImage', this.state.files[0]);
-   data.append('filterName', this.refs.filter.value);
+   data.append('filterName', this.state.filterName.split(".")[0]);
    this.setState({ loading: true }, () => {
-   var dbx = new Dropbox({ accessToken: '56Avmht1CGkAAAAAAAABS8gYYmjrmVC5HZc6ThJbMT168Y1MrWhY0wjfXUln64i4' });
    var re = /\S+@\S+\.\S+/;
    if(this.state.files[0]===undefined || !re.test(this.refs.email.value)) {this.setState({alert:true,loading:false})}
     else{
-   dbx.filesUpload({path: '/prisma/input/'+this.refs.email.value+"/"+this.refs.filter.value+"_"+uuid.v4()+"_"+this.state.files[0].name, contents: this.state.files[0]})
+   dbx.filesUpload({path: '/prisma/input/'+this.refs.email.value+"/"+this.state.filterName.split(".")[0]+"_"+uuid.v4()+"_"+this.state.files[0].name, contents: this.state.files[0]})
    .then(response=> {
         this.setState({
          loading: false,
@@ -64,39 +80,45 @@ onDrop(files) {
     this.setState({ alert: false });
   }
 
+onSlideChange(e) {
+    console.log('Item`s position during a change: ', e.item);
+    console.log('Slide`s position during a change: ', e.slide);
+  }
+ 
+onSlideChanged(e) {
+    console.log('Item`s position after changes: ', e.item);
+    console.log('Slide`s position after changes: ', e.slide);
+  }
+ 
+ selectFilter(filter) {
+  console.log(filter);
+  this.setState({filterName:filter});
+
+ }
 
 	render() {
-		let filterOptions = this.props.filters.map(filter => {
-      return <option key={filter} value={filter}>{filter}</option>
+    let filterItems = this.state.filters.map(filter=>{
+       return(<Carousel.Item key={filter.name}><div className="container"><img className="img-carousel" src={filter.img} onClick={this.selectFilter.bind(this, filter.name)}/><div className="centered">{filter.name.split('.')[0]}</div></div></Carousel.Item>);
     });
+  
+ 
     const {loading, alert, response } = this.state;
 
 		return(
       <div className="SelectUseCase">
-        {alert? <div className="pull-right">{JSON.stringify(response.id)!==undefined? <Alert bsStyle="success"  onDismiss={this.handleDismiss.bind(this)}>"File Upload Successful!!"</Alert>:<Alert bsStyle="danger" onDismiss={this.handleDismiss.bind(this)}>"Oops! Well this is embarassing. Please try again."</Alert>}</div>:<span></span>}
-        <br/>
-        <h3>Neural Style uploader </h3>
-         {loading ? <LoadingSpinner />:<div></div>}
       <div className="row">
-
-        <div className="col-sm-4">
-        <h3>Step 1.</h3>
-          <div className="form-group">
-            <label for="filter"> <h4>Select Filter:</h4> </label> 
-            <select className="form-control" ref="filter">
-                      {filterOptions}
-                  </select>
-          </div>
-          <div className="form-group">
-            <label><h4> Enter Email Address:</h4></label>
-            <input type="text" className="form-control" ref="email"/>
-          </div>
+        <div className="col-sm-4"><h2>NeurAl Style Transfer Yourself</h2></div>
+        <div className="pull-right col-sm-8">
+        {alert? <div className="col-sm-8">{JSON.stringify(response.id)!==undefined? <Alert bsStyle="success"  onDismiss={this.handleDismiss.bind(this)}>"File Upload Successful!!<br/> We will email your image shortly."</Alert>:<Alert bsStyle="danger" onDismiss={this.handleDismiss.bind(this)}>"Oops! Well this is embarassing. Please try again."</Alert>}</div>:<span></span>}
         </div>
-
-         <div className="col-sm-2"></div>
-         <div className="col-sm-4">
-        <div className="dropzone">
-        <h3> Step 2. Upload Your Image / Video:</h3> 
+      <br/><br/>
+      </div>
+     <h1> {loading ? <LoadingSpinner />:<div></div>}</h1>
+      <div className="row">
+        <div className="col-sm-4">
+         <div className="dropzone">
+            <label> <h3> Step 1.</h3></label>
+          <h4>Upload Your Image / Video: </h4>
           <Dropzone onDrop={this.onDrop.bind(this)} className=" text-center dropzone-override">
             <h4>Try dropping a file here  <br/><br/>
             <i className="push-down fa fa-upload fa-lg"/> <br/><br/>
@@ -109,17 +131,32 @@ onDrop(files) {
           </ul>
           </Dropzone>
         </div>
-			</div>
-      </div>
+        </div>
+         <div className="col-sm-5">
+         <h3>Step 2.</h3>
+         <label>  <h4>Select Filter: {this.state.filterName.split(".")[0]}</h4></label>
+      <Carousel style={{height:"200px"}}>
+        {filterItems}
+      </Carousel>
+         </div>
+
+         <div className="col-sm-3">
+         <h3>Step 3.</h3>
+          <div className="form-group">
+            <label><h4> Enter Email Address:</h4></label>
+            <input type="text" className="form-control" ref="email"/>
+          </div>
+        </div>
+       </div>
       <br/>
       <div className="row ">
-      <div className="col-sm-3 pull-right">
-        <button className="btn btn-primary" type="submit" onClick={this.handleSubmit.bind(this)}> Submit </button>  
+      <div className="col-sm-6 pull-right">
+        <Button bsStyle="success" bsSize="large" onClick={this.handleSubmit.bind(this)}> Submit </Button>  
       </div>
       </div>
       </div>
 		);
 	}
 }
-
+var dbx = new Dropbox({ accessToken: '56Avmht1CGkAAAAAAAABS8gYYmjrmVC5HZc6ThJbMT168Y1MrWhY0wjfXUln64i4' }); 
 export default Form;
--- prisma/App/web-app/src/components/navigator.js
+++ prisma/App/web-app/src/components/navigator.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
 import 'font-awesome/css/font-awesome.min.css'; 
-import {Nav, Navbar, NavItem, MenuItem,Dropdown} from 'react-bootstrap';
+import {Nav, Navbar, NavItem} from 'react-bootstrap';
 
 class Navigator extends Component{
 
@@ -21,39 +21,13 @@ handleSelect(eventKey) {
 
 render() {
   return(
-<Navbar inverse collapseOnSelect>
+<Navbar style={{backgrounColor:"black"}} collapseOnSelect>
   <Navbar.Toggle />
   <Navbar.Collapse>
     <Nav activekey={this.state.activekey}  onSelect={this.handleSelect.bind(this)}>
-      <LinkContainer to='/demo/'>
+      <LinkContainer to='/'>
       <NavItem eventKey={1}> Upload Image / Video</NavItem>
       </LinkContainer>
-    </Nav>
-    <Nav pullRight>
-    
-    <Dropdown id="dropdown-custom-1">
-    <Dropdown.Toggle useAnchor={true} noCaret inverse>
-       <div className="square-button inbox big-icon"> <i className="fa fa-inbox fa-3x"></i></div>
-     </Dropdown.Toggle>
-      <Dropdown.Menu>   
-        <MenuItem eventKey={4.1}>Update Profile</MenuItem>
-        <MenuItem eventKey={4.2}>Setting</MenuItem>
-        <MenuItem divider />
-        <MenuItem eventKey={4.3}>Logout</MenuItem>
-      </Dropdown.Menu>  
-       </Dropdown>
-    <span> &nbsp;&nbsp;&nbsp;</span>
-    <Dropdown id="dropdown-custom-2">
-    <Dropdown.Toggle useAnchor={true} noCaret inverse>
-       <div className="square-button inbox big-icon"> <i className="fa fa-user fa-3x"></i></div>
-     </Dropdown.Toggle>
-      <Dropdown.Menu>   
-        <MenuItem eventKey={5.1}>Update Profile</MenuItem>
-        <MenuItem eventKey={5.2}>Setting</MenuItem>
-        <MenuItem divider />
-        <MenuItem eventKey={5.3}>Logout</MenuItem>
-      </Dropdown.Menu>  
-      </Dropdown>
     </Nav>
   </Navbar.Collapse>
   </Navbar>
